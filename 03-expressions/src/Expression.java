07fd1ff791fe0cae989d41bce75d3dc61f074310
Ethan Yu <sohbmpethan@gmail.com>
2020-07-13 15:58:32 +0900

    03-expressions

---
+++ 03-expressions/src/Expression.java
@@ -0,0 +1,11 @@
+public class Expression {
+    public static void main(String[] args) {
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
