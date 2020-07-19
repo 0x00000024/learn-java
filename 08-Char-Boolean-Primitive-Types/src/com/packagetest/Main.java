3f32cf1e9ad0439489ab06216c1de6c8d88da4f5
Ethan Yu <sohbmpethan@gmail.com>
2020-07-20 00:02:26 +0900

    08-Char-Boolean-Primitive-Types

---
+++ 08-Char-Boolean-Primitive-Types/src/com/packagetest/Main.java
@@ -0,0 +1,17 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'E';
+        char myUnicodeChar = '\u0045';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+    }
+}
