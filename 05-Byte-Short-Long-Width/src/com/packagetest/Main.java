df570356cddce45df9e747b54dce5e15eab1759e
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 21:19:25 +0900

    05-Byte-Short-Long-Width

---
+++ 05-Byte-Short-Long-Width/src/com/packagetest/Main.java
@@ -0,0 +1,31 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // byte: 8 bits
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        // short: 16 bits
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        // int: 32 bits
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Int Minimum Value = " + myMinIntValue);
+        System.out.println("Int Maximum Value = " + myMaxIntValue);
+
+        // long: 64 bits
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+    }
+}
