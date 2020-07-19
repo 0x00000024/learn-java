0c9ca6cfb1fbdd838fabafbe34e71d06275a3651
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 23:49:58 +0900

    01-Practice

---
+++ 07-Float-Double-Primitive-Types/01-Practice/src/com/packagetest/Main.java
@@ -0,0 +1,13 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double pounds = 5.0d;
+        double kilograms = 0.0d;
+        kilograms = pounds * 0.45359237d;
+        System.out.println(kilograms);
+
+    }
+}
