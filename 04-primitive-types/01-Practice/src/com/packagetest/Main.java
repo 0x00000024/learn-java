feb90ebc751a35c69a78d23e4bb35cec17451e50
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 21:43:23 +0900

    01-Practice

---
+++ 04-primitive-types/01-Practice/src/com/packagetest/Main.java
@@ -0,0 +1,17 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+
+        short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
+
+    }
+}
