ec0b597b632751dd691bdcf4de569d0836ef9614
Ethan Yu <sohbmpethan@gmail.com>
2020-07-23 21:13:20 +0900

    01-Keywords-Expressions

---
+++ 04-Expressions-Statements-Code-blocks-Methods/01-Keywords-Expressions/src/com/packagetest/Main.java
@@ -0,0 +1,21 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+    }
+}
