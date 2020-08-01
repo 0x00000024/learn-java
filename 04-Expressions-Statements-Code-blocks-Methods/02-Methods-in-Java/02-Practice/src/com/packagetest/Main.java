3c6064826e981f3c8f75de9876b5453dc50bea66
Ethan Yu <sohbmpethan@gmail.com>
2020-08-01 22:08:26 +0900

    02-Practice

---
+++ 04-Expressions-Statements-Code-blocks-Methods/02-Methods-in-Java/02-Practice/src/com/packagetest/Main.java
@@ -0,0 +1,13 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
+
+}
---
+++ 04-Expressions-Statements-Code-blocks-Methods/02-Methods-in-Java/02-Practice/src/com/packagetest/SpeedConverter.java
@@ -0,0 +1,23 @@
+package com.packagetest;
+
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609d);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid value");
+        }
+        else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
