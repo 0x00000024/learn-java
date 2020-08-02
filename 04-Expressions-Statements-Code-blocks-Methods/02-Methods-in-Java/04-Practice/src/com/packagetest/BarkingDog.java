4090da878dbab6047a17d0c09f3e50874d116d00
Ethan Yu <sohbmpethan@gmail.com>
2020-08-02 11:21:41 +0900

    05-Practice

---
+++ 04-Expressions-Statements-Code-blocks-Methods/02-Methods-in-Java/04-Practice/src/com/packagetest/BarkingDog.java
@@ -0,0 +1,16 @@
+package com.packagetest;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+
+        if ((hourOfDay < 8 || hourOfDay > 22) && barking == true) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ 04-Expressions-Statements-Code-blocks-Methods/02-Methods-in-Java/04-Practice/src/com/packagetest/Main.java
@@ -0,0 +1,11 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+
+    }
+
+}
