712146aca410e29423dcda97bd881a3d32092bae
Ethan Yu <sohbmpethan@gmail.com>
2020-08-06 17:59:13 +0900

    04-Parsing-Values-from-String

---
+++ 05-Control-Flow-Statements/04-Parsing-Values-from-String/src/com/packagetest/Main.java
@@ -0,0 +1,19 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String numberAsString = "2020";
+        System.out.println("numberAsString= " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+    }
+}
