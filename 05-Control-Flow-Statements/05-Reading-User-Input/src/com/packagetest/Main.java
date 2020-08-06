ab98c2063a61c42e0829ea02e1757a6d1d676f36
Ethan Yu <sohbmpethan@gmail.com>
2020-08-06 18:06:49 +0900

    05-Reading-User-Input

---
+++ 05-Control-Flow-Statements/05-Reading-User-Input/src/com/packagetest/Main.java
@@ -0,0 +1,25 @@
+package com.packagetest;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth:");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        int age = 2020 - yearOfBirth;
+
+        System.out.println("Your name is " + name + ", and you are " + age + " years old");
+
+        scanner.close();
+    }
+}
