040815167cd015e2cebaa1144a74ae3275e9dff3
Ethan Yu <sohbmpethan@gmail.com>
2020-08-10 03:42:56 +0900

    03-Encapsulation

---
+++ 07-OOP-Composition-Encapsulation-Polymorphism/03-Encapsulation/src/com/packagetest/Main.java
@@ -0,0 +1,12 @@
+package com.packagetest;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Printer printer = new Printer(50, 4, true);
+        System.out.println("Initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+    }
+}
---
+++ 07-OOP-Composition-Encapsulation-Polymorphism/03-Encapsulation/src/com/packagetest/Printer.java
@@ -0,0 +1,44 @@
+package com.packagetest;
+
+public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
+        if (tonerLevel >= 0 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.pagesPrinted = pagesPrinted;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (this.tonerLevel > -1 && this.tonerLevel <= 100) {
+            if (this.tonerLevel + tonerAmount > 100) {
+                return -1;
+            }
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
+        } else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.duplex) {
+            pagesPrinted /= 2;
+            System.out.println("Printing in duplex mode");
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
