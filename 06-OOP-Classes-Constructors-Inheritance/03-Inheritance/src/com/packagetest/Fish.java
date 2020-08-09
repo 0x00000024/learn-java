c097cad7c1fe208239b2ccffc403fe351c7eefd7
Ethan Yu <sohbmpethan@gmail.com>
2020-08-09 22:08:38 +0900

    03-Inheritance

---
+++ 06-OOP-Classes-Constructors-Inheritance/03-Inheritance/src/com/packagetest/Fish.java
@@ -0,0 +1,16 @@
+package com.packagetest;
+
+public class Fish {
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+
+//        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+}
