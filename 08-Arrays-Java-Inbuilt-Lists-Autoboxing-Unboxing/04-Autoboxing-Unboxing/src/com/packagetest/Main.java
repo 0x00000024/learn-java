737d83dbb645921a598476d3d06ab70f74a4f71c
Ethan Yu <sohbmpethan@gmail.com>
2020-08-10 20:11:55 +0900

    04-Autoboxing-Unboxing

---
+++ 08-Arrays-Java-Inbuilt-Lists-Autoboxing-Unboxing/04-Autoboxing-Unboxing/src/com/packagetest/Main.java
@@ -0,0 +1,44 @@
+package com.packagetest;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+class IntClass {
+
+    private int myValue;
+
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] strArray = new String[10];
+        int[] intArray = new int[10];
+
+        ArrayList<String> strArrayList = new ArrayList<String>();
+        strArrayList.add("Tim");
+
+        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
+        intClassArrayList.add(new IntClass(54));
+
+        Integer integer = new Integer(54);
+        Double doubleValue = new Double(12.25);
+
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+        for (int i = 0; i <= 10; i++) {
+            intArrayList.add(i);
+        }
+
+    }
+}
