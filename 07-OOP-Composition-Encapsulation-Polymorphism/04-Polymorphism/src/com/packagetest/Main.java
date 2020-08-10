2c51d1feef1b0fb5c6012a71162385eb88a7ed0f
Ethan Yu <sohbmpethan@gmail.com>
2020-08-10 11:27:59 +0900

    04-Polymorphism

--- 07-OOP-Composition-Encapsulation-Polymorphism/04-Polymorphism/src/com/packagetest/Main.java
+++ 07-OOP-Composition-Encapsulation-Polymorphism/04-Polymorphism/src/com/packagetest/Main.java
@@ -18,6 +18,7 @@ class Jaws extends Movie {
         super("Jaws");
     }
 
+    @Override
     public String plot() {
         return "A shark eats lots of people";
     }
@@ -29,15 +30,68 @@ class IndependenceDay extends Movie {
         super("IndependenceDay");
     }
 
-    //TODO
     @Override
     public String plot() {
-        return super.plot();
+        return "Aliens attempt to take over planet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze.";
+    }
+}
+
+class StarWars extends Movie {
+
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the universe";
+    }
+}
+
+class Forgetable extends Movie {
+
+    public Forgetable() {
+        super("Forgetable");
     }
 }
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Movie movie = randomMovie();
+        System.out.println(movie.plot());
+    }
+
+    public static Movie randomMovie() {
+
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+            default:
+                return null;
+        }
     }
 }
